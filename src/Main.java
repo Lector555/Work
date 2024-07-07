2f08b4b64b7bc897a7a746d1b4bbeb3e9458d8ce
Lector555 <166312223+Lector555@users.noreply.github.com>
2024-07-07 23:05:42 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Введите выражение в формате x + y");
-        Scanner scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
-        String [] a = str.split(" ");
-        if (a.length != 3){
+        String[] a = str.split(" ");
+        if (a.length != 3) {
             throw new RuntimeException();
         }
         String num1 = a[0];
@@ -17,21 +17,21 @@ public class Main {
 
 
         String num1type;
-        if (Character.isDigit(num1.charAt(0))){
+        if (Character.isDigit(num1.charAt(0))) {
             num1type = "arab";
-        }else {
+        } else {
             num1type = "rome";
         }
         String num2type;
-        if (Character.isDigit(num2.charAt(0))){
+        if (Character.isDigit(num2.charAt(0))) {
             num2type = "arab";
-        }else {
+        } else {
             num2type = "rome";
         }
-        if (!num1type.equals(num2type)){
+        if (!num1type.equals(num2type)) {
             throw new RuntimeException();
         }
-        if (num1type.equals("rome")){
+        if (num1type.equals("rome")) {
             num1 = Converter.converter(num1);
             num2 = Converter.converter(num2);
         }
@@ -40,21 +40,18 @@ public class Main {
         if (num1Iteger > 10
                 || num2Iteger > 10
                 || num1Iteger < 0
-                || num2Iteger < 0){
+                || num2Iteger < 0) {
             throw new RuntimeException();
         }
 
 
-
         int result = Switch.calculated(num1Iteger, num2Iteger, oper);
         String result2;
-        if (num1type.equals("rome")){
+        if (num1type.equals("rome")) {
             result2 = Converter.romeToArab(String.valueOf(result));
-        }else {
-            result2=String.valueOf(result);
+        } else {
+            result2 = String.valueOf(result);
         }
         System.out.println(result2);
     }
-
-
 }
